c349f7709bf77cddba91f7ea4526377b95a6eed0
Худышка К <skinny.developer@gmail.com>
2023-01-26 16:40:04 +0300

    -//-

---
+++ English/app/src/main/java/com/example/hudihka/english/Words/WordsEndpoint.kt
@@ -0,0 +1,15 @@
+package com.example.hudihka.english.Words
+
+enum class WordsEndpoint {
+    ALL, FAVORIT;
+
+    companion object {
+        fun from(index: Int): WordsEndpoint {
+            if (index == 0) {
+                return ALL
+            } else {
+                return FAVORIT
+            }
+        }
+    }
+}
